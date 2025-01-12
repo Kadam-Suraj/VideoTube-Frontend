1fbeeb741601ce9aaf719aae43f60c591cb84eb9
Kadam-Suraj <surajkadam381@gmail.com>
2025-01-12 10:06:44 +0530

    feat: enhance Error handling in Home component; display 401 error for unauthorized users
    Signed-off-by: Kadam-Suraj <surajkadam381@gmail.com>

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -5,7 +5,7 @@ import PropTypes from "prop-types"
 const Error = ({ code = 404, data }) => {
     const errors = {
         404: {
-            title: "Oops!",
+            title: "404!",
             message: "Page not found",
             fallback: "Go back",
             link: "/"
@@ -48,17 +48,17 @@ const Error = ({ code = 404, data }) => {
     }
 
     return (
-        <div className='flex flex-col items-center justify-center p-5 sm:h-[calc(100vh-6.5rem)] h-[calc(100vh-10.5rem)] md:flex-row sm:space-x-10'>
+        <div className='flex flex-wrap items-center justify-center w-full gap-5 p-5 my-20'>
             <img src={`/404.svg`} className="w-4/5 min-w-72 sm:min-w-96 md:w-2/5" alt="NotFound" />
             <div className="flex flex-col items-center justify-center space-y-5">
                 <h1 className="text-5xl font-bold md:text-7xl" aria-label={`Error Code: ${code}`}>{data?.title || error.title}</h1>
-                <span className="text-xl font-semibold">Error: {data?.message || error.message}</span>
+                <span className="text-xl font-semibold text-center">Error: {data?.message || error.message}</span>
                 <span className="flex items-center gap-5">
                     <NavLink to={data?.link || error.link}>
                         <Button>{data?.fallback || error.fallback}</Button>
                     </NavLink>
                     {
-                        data?.fallback !== "Go back" && error.fallback !== "Go back" &&
+                        data?.fallback !== "Go back" && error.fallback !== "Go back" && window.location.pathname !== "/" &&
                         <NavLink to="/">
                             <Button>Home</Button>
                         </NavLink>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,20 @@
 import ShowVideos from "@/components/Video/ShowVideos"
+import { useAuth } from "@/context/AuthContext"
+import Error from "./Error";
 
 const Home = () => {
+    const { isLoggedIn } = useAuth();
 
     return (
         <>
             <section className="">
                 <div>
-                    < ShowVideos />
+                    {
+                        isLoggedIn ?
+                            < ShowVideos />
+                            :
+                            <Error code={401} />
+                    }
                 </div>
             </section >
         </>
